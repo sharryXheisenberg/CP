5bac6703032a5b476b5c40def0a95896c1555157
sharryXheisenberg <pandesaurabh4596@gmail.com>
2025-08-28 20:14:45 +0530

    updates ones

---
+++ codewars/The_highest_profitwins.cpp
@@ -0,0 +1,19 @@
+#include <utility>
+#include <vector>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+pair<int, int> min_max(const vector<int>& arr) {
+    if (arr.size() == 1) return {arr[0], arr[0]};
+    int mn = *min_element(arr.begin(), arr.end());
+    int mx = *max_element(arr.begin(), arr.end());
+    return {mn, mx};
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    vector<int>arr = {2334454,5};
+    pair<int,int>k = min_max(arr);
+    cout<<k.first<<" "<<k.second;
+}
---
+++ lc/max_area_of_reactangle_diagonal.cpp
@@ -0,0 +1,21 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// class Solution {
+// public:
+//     int areaOfMaxDiagonal(vector<vector<int>>& dimensions) {
+        
+//     }
+// };
+
+// int main(){
+//     int max = 1;
+//     vector<vector<int>>dimensions = {{9,3},{8,6}};
+//     for(int i=0;i<dimensions[0].size();i++){
+//         for(int j=0;j<dimensions[0].size();j++){
+//                 max = dimensions[i][j]<<
+
+//         }
+//     }
+//     return 0;
+// }
