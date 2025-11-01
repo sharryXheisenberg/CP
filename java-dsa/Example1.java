91f0b904c1ed33be9c7cb20bfd8e9d3b893d765e
sharryXheisenberg <pandesaurabh4596@gmail.com>
2025-11-01 21:58:11 +0530

    Java learning

---
+++ java-dsa/Example1.java
@@ -0,0 +1,12 @@
+public class Example1{
+    public static void main(String[] args){
+        int var1 = 1024;
+        int var2;
+        double x = 10.0;
+        System.out.println("Var1 contains" +var1);
+        var2 = var1/2;
+        System.out.println("var2 contains var1/2:"+var2);
+        System.out.println("Value of var1 and var2 after division " + var1/var2);
+        System.out.println("Value of floating / double data type " + x/4);
+    }
+};
