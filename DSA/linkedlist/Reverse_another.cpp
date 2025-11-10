08057b53218818124d27bcc60d5e4d8aa57368d8
sharryXheisenberg <pandesaurabh4596@gmail.com>
2025-11-10 09:23:00 +0530

    Updated learning of LinkedList

---
+++ DSA/linkedlist/Reverse_another.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define en endl
+#define ll long long 
+
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+       ListNode* prev = NULL;
+       ListNode* Next = NULL;
+       ListNode* curr = head;
+
+       if(head==NULL || head->next==nullptr){
+        return head;
+       }
+
+       while(curr!=NULL){
+        Next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = Next;
+       }
+       return prev;
+    }
+};
+
+
+int main(){
+
+}
