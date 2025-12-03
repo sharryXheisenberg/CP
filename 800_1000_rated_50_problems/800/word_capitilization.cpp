8b431ea66dea083d333afd57943c09e44002a54e
sharryXheisenberg <pandesaurabh4596@gmail.com>
2025-12-03 12:06:58 +0530

    Random 800 rated CF problem

---
+++ 800_1000_rated_50_problems/800/word_capitilization.cpp
@@ -0,0 +1,21 @@
+/* 
+ * Author - Balerion_The_second
+ */
+
+
+#include<bits/stdc++.h>
+#define ll long long
+#define en endl
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    string s;
+    cin>>s;
+    s[0] = toupper(s[0]);
+    cout<<s<<en;
+
+    return 0;
+}
+
