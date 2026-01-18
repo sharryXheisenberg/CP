9703579a29cf71b3425d8b5369d41a65618e2aa5
sharryXheisenberg <pandesaurabh4596@gmail.com>
2026-01-18 21:20:28 +0530

    Easy CP problem but with tricky explaination

---
+++ cf_contest/perfect_root_one.cpp
@@ -0,0 +1,24 @@
+/* * (▀̿Ĺ̯▀̿ ̿) Author - Balerion_The_second  (▀̿Ĺ̯▀̿ ̿)
+ */
+
+#include<bits/stdc++.h>
+#define ll long long
+#define en endl
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        for(int i = 1; i <= n; i++) {
+            cout << i << " ";
+        }
+        cout << en;
+    }
+    return 0;
+}
