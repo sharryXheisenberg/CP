c822bb0647170a131d6667255955ca51f96e0465
sharryXheisenberg <pandesaurabh4596@gmail.com>
2026-04-05 09:23:40 +0530

    Easy CF 800 rated problem

---
+++ 800_1000_rated_50_problems/800/the_67th_integer_problem.cpp
@@ -0,0 +1,26 @@
+/* 
+ *   Author - Balerion_The_second  
+ */
+
+#include<bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define vec vector<ll>
+ll MOD = (7 + (1e9));
+#define en endl
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+   
+    ll t;
+    cin>>t;
+    while(t--){
+        ll x;
+        cin>>x;
+        cout<<x<<en;
+    }
+    return 0;
+    
+}
